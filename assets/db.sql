4a711e4449d91d38ab9d85c8edc7ed9f3acd6b7d
theo <37940969+theo52130@users.noreply.github.com>
2024-09-25 09:17:34 +0200

    Update db.sql

--- assets/db.sql
+++ assets/db.sql
@@ -1,59 +1,38 @@
+CREATE DATABASE IF NOT EXISTS test_cube_trois;
+
 USE test_cube_trois;
 
-CREATE TABLE entreprises (
+CREATE TABLE comptes (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nom VARCHAR(255) NOT NULL,
-    adresse TEXT
-);
-
-CREATE TABLE clients (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(255) NOT NULL,
-    email VARCHAR(255),
-    entreprise_id INT,
-    FOREIGN KEY (entreprise_id) REFERENCES entreprises (id)
+    email VARCHAR(255) NOT NULL UNIQUE,
+    adresse VARCHAR(255) NOT NULL,
+    email_entreprise VARCHAR(255),
+    siret BIGINT,
+    password VARCHAR(255) NOT NULL,
+    role ENUM('client', 'employer', 'admin') NOT NULL
 );
 
 CREATE TABLE factures (
     id INT AUTO_INCREMENT PRIMARY KEY,
     client_id INT,
-    date_creation DATE,
-    total DECIMAL(10, 2),
+    date_creation DATE NOT NULL DEFAULT CURRENT_DATE,
+    total DECIMAL(10, 2) NOT NULL,
     etat ENUM('payée', 'non payée') DEFAULT 'non payée',
-    FOREIGN KEY (client_id) REFERENCES clients (id)
+    FOREIGN KEY (client_id) REFERENCES comptes (id) ON DELETE CASCADE
 );
 
 CREATE TABLE produits (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    facture_id INT,
-    description TEXT,
-    prix_unitaire DECIMAL(10, 2),
-    quantite INT,
-    FOREIGN KEY (facture_id) REFERENCES factures (id)
+    description TEXT NOT NULL,
+    prix_unitaire DECIMAL(10, 2) NOT NULL,
+    quantite INT NOT NULL
 );
 
 CREATE TABLE factures_produits (
     facture_id INT,
     produit_id INT,
-    FOREIGN KEY (facture_id) REFERENCES factures (id),
-    FOREIGN KEY (produit_id) REFERENCES produits (id)
-);
-
-CREATE TABLE utilisateurs (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nom VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    mot_de_passe VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE roles (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nom VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE utilisateurs_rôles (
-    utilisateur_id INT,
-    rôle_id INT,
-    FOREIGN KEY (utilisateur_id) REFERENCES utilisateurs (id),
-    FOREIGN KEY (rôle_id) REFERENCES roles (id)
+    PRIMARY KEY (facture_id, produit_id),
+    FOREIGN KEY (facture_id) REFERENCES factures (id) ON DELETE CASCADE,
+    FOREIGN KEY (produit_id) REFERENCES produits (id) ON DELETE CASCADE
 );
